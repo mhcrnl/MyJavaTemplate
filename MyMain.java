7da94aeac0046d8b8ed21dd9947ebbd6433d9485
Mihai Cornel <mhcrnl@gmail.com>
2022-09-24 21:17:09 +0300

    First commit

---
+++ MyMain.java
@@ -0,0 +1,7 @@
+imput java.io.*;
+
+public class MyMain {
+    public static void main (String args[]) {
+        System.out.println(" Hello World! ");
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Proiecte Java, JavaFx
+
+## Dezvoltare:
+
+OS: Fedora 36
+Java : 11, 17
+JavaFx: 
+Netbeans 15
